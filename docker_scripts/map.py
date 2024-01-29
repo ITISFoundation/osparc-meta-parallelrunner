d1448a9349df03fc9d975d1d48d12a059632825d
Werner Van Geit <werner.vangeit@epfl.ch>
2024-01-29 10:29:33 +0100

    Testing permissions

--- docker_scripts/map.py
+++ docker_scripts/map.py
@@ -1,5 +1,7 @@
 import json
 import os
+import time
+
 import pathlib as pl
 import logging
 
@@ -7,6 +9,8 @@ logging.basicConfig(
     level=logging.INFO, format="[%(filename)s:%(lineno)d] %(message)s"
 )
 logger = logging.getLogger(__name__)
+
+POLLING_INTERVAL = 1  # second
 
 import osparc
 import osparc_client
@@ -33,10 +37,10 @@ class MapRunner:
 
         self.input_path = input_path  # path where osparc write all our input
         self.template_id = "f448d118-bc54-11ee-ba85-02420a000022"
+        self.key_values_path = self.input_path / "key_values.json"
 
     def setup(self):
         """Setup the Python Runner"""
-        logger.info("Setting up map ...")
         self.osparc_cfg = osparc.Configuration(
             host="10.43.103.149.nip.io:8006",
             username="test_T1QyAxKBUX",
@@ -48,11 +52,18 @@ class MapRunner:
     def start(self):
         """Start the Python Runner"""
         logger.info("Starting map ...")
+
+        while not self.key_values_path.exists():
+            logger.info("Waiting for key_values.json to exist ...")
+            time.sleep(POLLING_INTERVAL)
+
+        # logger.info(json.loads(self.input_path.read_text()))
+
         new_job = self.studies_api.create_study_job(
             "02621c5a-bbba-11ee-ba85-02420a000022", job_inputs={"values": {}}
         )
 
-        print(
+        logger.info(
             self.studies_api.start_study_job(
                 study_id=self.template_id, job_id=new_job.id
             )
