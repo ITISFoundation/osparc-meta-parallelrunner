b37bcc6ebd2645b535a6a6328b7a4886ae39267a
Werner Van Geit <werner.vangeit@epfl.ch>
2024-01-30 13:42:48 +0100

    Working with json input and output files

--- docker_scripts/map.py
+++ docker_scripts/map.py
@@ -1,8 +1,9 @@
 import json
 import os
 import time
-import pathos
 
+import tempfile
+import zipfile
 
 import pathlib as pl
 import logging
@@ -11,6 +12,8 @@ logging.basicConfig(
     level=logging.INFO, format="[%(filename)s:%(lineno)d] %(message)s"
 )
 logger = logging.getLogger(__name__)
+
+import pathos
 
 POLLING_INTERVAL = 1  # second
 TEMPLATE_ID_KEY = "input_0"
@@ -41,15 +44,14 @@ class MapRunner:
         """Constructor"""
 
         self.input_path = input_path  # path where osparc write all our input
-        self.template_id = "f448d118-bc54-11ee-ba85-02420a000022"
         self.key_values_path = self.input_path / "key_values.json"
 
     def setup(self):
         """Setup the Python Runner"""
         self.osparc_cfg = osparc.Configuration(
             host="10.43.103.149.nip.io:8006",
-            username="test_T1QyAxKBUX",
-            password="0dsHA6zdYDNEtwNKsXZHBQq8eHuPbd",
+            username=os.environ["OSPARC_API_KEY"],
+            password=os.environ["OSPARC_API_SECRET"],
         )
         self.api_client = osparc.ApiClient(self.osparc_cfg)
         self.studies_api = osparc_client.StudiesApi(self.api_client)
@@ -62,7 +64,6 @@ class MapRunner:
 
         logger.info(f"User: {getpass.getuser()}, UID: {os.getuid()}")
         logger.info(f"Input path: {self.input_path.resolve()}")
-        logger.info(f"Input content: {list(self.input_path.rglob('*'))}")
 
         while not self.key_values_path.exists():
             logger.info("Waiting for key_values.json to exist ...")
@@ -72,32 +73,48 @@ class MapRunner:
 
         logger.info(f"Key/Values: {key_values}")
 
-        template_id = key_values[TEMPLATE_ID_KEY]["value"]
-        if template_id is None:
+        self.template_id = key_values[TEMPLATE_ID_KEY]["value"]
+        if self.template_id is None:
             raise ValueError("Template ID can't be None")
 
         n_of_workers = key_values[N_OF_WORKERS_KEY]["value"]
         if n_of_workers is None:
             raise ValueError("Number of workers can't be None")
 
-        input_parameters = key_values[INPUT_PARAMETERS_KEY]["value"]
-        if input_parameters is None:
-            raise ValueError("Input parameters can't be None")
+        input_parameters_dir_path = self.input_path / INPUT_PARAMETERS_KEY
 
+        json_list = list(input_parameters_dir_path.glob("*.json"))
+        if len(json_list) != 1:
+            raise ValueError(
+                f"More than 1 or no json file found in the input parameters path: {json_list}"
+            )
+
+        input_parameters_path = json_list[0]
+
+        input_parameters = json.loads(input_parameters_path.read_text())
+
+        logger.info(f"Evaluating: {input_parameters}")
+
+        logger.info(f"Starting {n_of_workers} workers")
         executor = pathos.pools.ThreadPool(nodes=n_of_workers)
 
-        job_id = self.studies_api.create_study_job(
-            study_id=self.template_id, job_inputs={"values": {}}
-        )
-        logger.info(job_id)
-
         def map_func(input):
-            logger.info(f"Running worker for: {input}")
+            logger.info(f"Running worker for input: {input}")
 
-            output = {}
+            output = {"input": input}
+
+            tmp_dir = tempfile.TemporaryDirectory()
+            tmp_dir_path = pl.Path(tmp_dir.name)
+            tmp_input_file_path = tmp_dir_path / "input.json"
+            tmp_input_file_path.write_text(json.dumps(input))
+
+            input_data_file = osparc.FilesApi(self.api_client).upload_file(
+                file=tmp_input_file_path
+            )
 
             job = self.studies_api.create_study_job(
-                study_id=self.template_id, job_inputs={"values": {}}
+                study_id=self.template_id,
+                job_inputs={"values": {"InputFile1": input_data_file}},
             )
 
             job_status = self.studies_api.start_study_job(
@@ -108,9 +125,8 @@ class MapRunner:
                 job_status.state != "SUCCESS" and job_status.state != "FAILED"
             ):
                 job_status = self.studies_api.inspect_study_job(
-                    study_id=template_id, job_id=job.id
+                    study_id=self.template_id, job_id=job.id
                 )
-                # logger.info(f"'{job_status.state}'")
                 time.sleep(1)
 
             output["status"] = job_status.state
@@ -118,29 +134,43 @@ class MapRunner:
             if job_status.state == "FAILED":
                 output["results"] = None
             else:
-                output["results"] = self.studies_api.get_study_job_outputs(
-                    study_id=template_id, job_id=job.id
+                results = self.studies_api.get_study_job_outputs(
+                    study_id=self.template_id, job_id=job.id
                 ).results
+
+                output["results"] = {}
+                for probe_name, probe_output in results.items():
+                    if isinstance(
+                        probe_output, osparc_client.models.file.File
+                    ):
+                        output_file = pl.Path(
+                            osparc.FilesApi(self.api_client).download_file(
+                                probe_output.id
+                            )
+                        )
+                        with zipfile.ZipFile(output_file, "r") as zip_file:
+                            file_results_path = zipfile.Path(
+                                zip_file, at="output.json"
+                            )
+                            file_results = json.loads(
+                                file_results_path.read_text()
+                            )
+
+                        output["results"][probe_name] = file_results
+                    else:
+                        output["results"][probe_name] = probe_output
 
             self.studies_api.delete_study_job(
                 study_id=self.template_id, job_id=job.id
             )
 
-            logger.info("Worker has finished")
+            logger.info(f"Worker has finished with output: {output}")
 
             return output
 
         map_args = [parameter_set for parameter_set in input_parameters]
 
         print(list(executor.map(map_func, map_args)))
-
-        """
-        logger.info(
-            self.studies_api.start_study_job(
-                study_id=self.template_id, job_id=new_job.id
-            )
-        )
-        """
 
     def teardown(self):
         logger.info("Closing map ...")
