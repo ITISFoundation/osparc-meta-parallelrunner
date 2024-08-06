73e41e67d08d3e0813c35830d621153775753b8d
Werner Van Geit <werner.vangeit@epfl.ch>
2024-08-06 16:02:30 +0200

    Rename config file to settings.json

--- docker_scripts/main.py
+++ docker_scripts/main.py
@@ -22,14 +22,14 @@ CONF_SCHEMA_KEY = "conf_json_schema"
 
 DEFAULT_FILE_POLLING_INTERVAL = 1  # second
 
-DEFAULT_NUMBER_OF_WORKERS = None
-DEFAULT_TEMPLATE_ID = None
+DEFAULT_NUMBER_OF_WORKERS = 1
+DEFAULT_TEMPLATE_ID = ""
 
 DEFAULT_MAX_JOB_CREATE_ATTEMPTS = 5
 DEFAULT_JOB_CREATE_ATTEMPTS_DELAY = 5
 DEFAULT_MAX_JOB_TRIALS = 5
 
-DEFAULT_JOB_TIMEOUT = None
+DEFAULT_JOB_TIMEOUT = 0
 
 MAX_N_OF_WORKERS = 10
 
@@ -58,9 +58,10 @@ def main():
 
             # Wait for and read the settings file
             logger.info(
-                f"Waiting for config file to appear at {settings.settings_file_path}"
+                f"Waiting for settings file to appear at {settings.settings_file_path}"
             )
             settings.read_settings_file()
+            logger.info("Settings file was read")
 
             # Create and start the maprunner
             maprunner = parallelrunner.ParallelRunner(settings)
@@ -79,14 +80,24 @@ def main():
 class ParallelRunnerDynamicSettings:
     def __init__(self):
         self._settings = self.ParallelRunnerMainSettings()
-        settings_schema = self._settings.model_json_schema()
         conf_json_schema_path = (
             self._settings.output_path / CONF_SCHEMA_KEY / "schema.json"
         )
+
+        settings_schema = self._settings.model_json_schema()
+
+        # Hide some settings from the user
+        for field_name in [
+            "max_number_of_workers",
+            "DY_SIDECAR_PATH_INPUTS",
+            "DY_SIDECAR_PATH_OUTPUTS",
+        ]:
+            settings_schema["properties"].pop(field_name)
+
         conf_json_schema_path.write_text(json.dumps(settings_schema, indent=2))
 
         self.settings_file_path = (
-            self._settings.input_path / INPUT_CONF_KEY / "parallelrunner.json"
+            self._settings.input_path / INPUT_CONF_KEY / "settings.json"
         )
 
     def __getattr__(self, name):
@@ -101,26 +112,34 @@ class ParallelRunnerDynamicSettings:
         self._settings = self._settings.parse_file(self.settings_file_path)
 
     class ParallelRunnerMainSettings(pydantic_settings.BaseSettings):
-        template_id: None | str = DEFAULT_TEMPLATE_ID
+        template_id: str = pyda.Field(default=DEFAULT_TEMPLATE_ID)
         max_number_of_workers: int = pyda.Field(
             MAX_N_OF_WORKERS, allow_mutation=False
         )
-        number_of_workers: None | int = pyda.Field(
-            DEFAULT_NUMBER_OF_WORKERS, gt=0, le=MAX_N_OF_WORKERS
+        number_of_workers: int = pyda.Field(
+            default=DEFAULT_NUMBER_OF_WORKERS, gt=0, le=MAX_N_OF_WORKERS
         )
-        batch_mode: bool = False
-        file_polling_interval: int = DEFAULT_FILE_POLLING_INTERVAL
+        batch_mode: bool = pyda.Field(default=False)
+        file_polling_interval: int = pyda.Field(
+            default=DEFAULT_FILE_POLLING_INTERVAL
+        )
         input_path: pyda.DirectoryPath = pyda.Field(
             alias="DY_SIDECAR_PATH_INPUTS"
         )
         output_path: pyda.DirectoryPath = pyda.Field(
             alias="DY_SIDECAR_PATH_OUTPUTS"
         )
-        max_job_trials: int = DEFAULT_MAX_JOB_TRIALS
-        file_polling_interval: int = DEFAULT_FILE_POLLING_INTERVAL
-        max_job_create_attempts: int = DEFAULT_MAX_JOB_CREATE_ATTEMPTS
-        job_create_attempts_delay: int = DEFAULT_JOB_CREATE_ATTEMPTS_DELAY
-        job_timeout: None | float = DEFAULT_JOB_TIMEOUT
+        max_job_trials: int = pyda.Field(default=DEFAULT_MAX_JOB_TRIALS, gt=0)
+        file_polling_interval: int = pyda.Field(
+            DEFAULT_FILE_POLLING_INTERVAL, gt=0
+        )
+        max_job_create_attempts: int = pyda.Field(
+            default=DEFAULT_MAX_JOB_CREATE_ATTEMPTS, gt=0
+        )
+        job_create_attempts_delay: int = pyda.Field(
+            default=DEFAULT_JOB_CREATE_ATTEMPTS_DELAY, gt=0
+        )
+        job_timeout: float = pyda.Field(default=DEFAULT_JOB_TIMEOUT, ge=0)
 
 
 if __name__ == "__main__":
